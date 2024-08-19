0e5c23172033b90864bf899f1b730fc5d62175cc
berkc <berkc@DESKTOP-PQO3M5E>
2024-08-19 16:50:42 +0300

    Add project files.

---
+++ Practice-One/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practice_One.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
