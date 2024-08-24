562f462bf3e91522cd6f6460478bb4ae6e784e89
Berk Çavuş <berkcavus38@outlook.com>
2024-08-24 21:04:14 +0300

    Basic CRUD Operations Completed

--- Practice-One/Controllers/CategoryController.cs
+++ Practice-One/Controllers/CategoryController.cs
@@ -24,8 +24,71 @@ namespace Practice_One.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Display order and name can not be the same");
+            }
+            if (obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is not a valid value");
+            }
+            if (ModelState.IsValid) {
             _db.Categories.Add(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category Has Been Added Successfully";
+             return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if(id  == 0 || id == null)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _db.Categories.Find(id);
+            if ( categoryFromDb == null)
+            {
+                return NotFound();
+            }
+                return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Has Been Edited Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category Has Been Deleted Successfully";
             return RedirectToAction("Index");
         }
     }
--- Practice-One/Models/Category.cs
+++ Practice-One/Models/Category.cs
@@ -4,9 +4,13 @@ namespace Practice_One.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(30)]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Only Letters are allowed.")]
         public string Name { get; set; }
+        [Range(1,100, ErrorMessage ="Please select a number between 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
